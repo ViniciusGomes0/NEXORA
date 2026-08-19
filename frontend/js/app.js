f0b0c86216f54bb42d483385d7cff2fe52ea0d33
Vinicius Gomes de Oliveira <miriamgisele33@gmail.com>
2026-08-19 12:48:17 -0300

    ajustes versao app

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -890,7 +890,7 @@ async function submitDeleteServer() {
 }
 
 function downloadApp() {
-    window.open('https://github.com/ViniciusGomes0/NEXORA/releases/download/v1.0.3/Nexora.Setup.1.0.3.exe', '_blank');
+    window.open('https://github.com/ViniciusGomes0/NEXORA/releases/download/v1.0.4/Nexora.Setup.1.0.3.exe', '_blank');
 }
 
 // Modals
