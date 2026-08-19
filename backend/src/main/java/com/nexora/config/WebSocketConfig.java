0d98016bc73080b91674b34b8fed443c90fe0b0b
Vinicius Gomes de Oliveira <miriamgisele33@gmail.com>
2026-08-19 00:36:08 -0300

    Ajuste imagens

--- backend/src/main/java/com/nexora/config/WebSocketConfig.java
+++ backend/src/main/java/com/nexora/config/WebSocketConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -29,5 +30,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureClientInboundChannel(ChannelRegistration registration) {
         registration.interceptors(webSocketAuthInterceptor);
+    }
+
+    @Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
+        registration.setMessageSizeLimit(20 * 1024 * 1024);   // 20MB inbound
+        registration.setSendBufferSizeLimit(20 * 1024 * 1024); // 20MB outbound
+        registration.setSendTimeLimit(30000);                  // 30s timeout
     }
 }
